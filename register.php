3162cb0d82583485525c90c25a48c223a045cfbe
netexpoarthur <aandre@netexpo.fr>
2021-09-22 14:36:31 +0000

    commit

--- register.php
+++ register.php
@@ -9,15 +9,31 @@ if (isset($_POST["mail"]) && isset($_POST["password"])) {
 
         if (filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
 
-            $verif_connection = user_password_verify($_POST["mail"], $_POST["password"]);
+            if (strlen($_POST["password"]) > 5) {
 
-            if ($verif_connection === true) {
+                if (preg_match('/[A-Z]/', $_POST["password"])) {
 
-                $_SESSION["mail"] = $_POST["mail"];
+                    if (user_verify_no_exist($_POST["mail"]) == false) {
 
-                echo '<script>window.location.href = "index.php"</script>';
+                        $sql = "INSERT INTO membres (mail, password) VALUES ('" . $_POST["mail"] . "', '" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "')";
+
+                        if (insert($sql) == true) {
+
+                            echo "user created";
+
+                        } else {
+                            $error = "Erreur de création de l'uttilisateur";
+                        }
+                    } else {
+                        $error = "L'uttilisateur existe déjà !";
+                    }
+
+                } else {
+                    $error = "Votre mot de passe doit contenir au moins une majuscule";
+                }
+
             } else {
-                $error = $verif_connection;
+                $error = "Mot de passe trop cours";
             }
 
         } else {
@@ -31,7 +47,6 @@ if (isset($_POST["mail"]) && isset($_POST["password"])) {
 
 
 
-
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -42,14 +57,11 @@ if (isset($_POST["mail"]) && isset($_POST["password"])) {
 
 </head>
 <body>
-
-
-
-<form method="POST" onsubmit="return verif_login()">
+  <form method="POST" onsubmit="return verif_register()">
 <div class="relative font-medium md:h-screen flex items-center content-center">
   <div class="mr-auto ml-auto w-full">
     <div class="w-full max-w-md mr-auto ml-auto mt-4 mb-1 text-center">
-      <h1 class="text-gray-800 block text-3xl font-extrabold font-title">Connexion</h1>
+      <h1 class="text-gray-800 block text-3xl font-extrabold font-title">Inscription</h1>
     </div>
     <div class="w-full max-w-md mr-auto ml-auto mt-4">
       <div class="bg-white shadow-lg rounded-md px-8 py-8 mb-4 ml-auto mr-auto">
@@ -62,26 +74,17 @@ if (isset($_POST["mail"]) && isset($_POST["password"])) {
           <label class="block text-gray-700 text-sm font-medium mb-2" for="username"> Mot de passe </label>
           <input class="shadow-sm appearance-none border border-gray-400 rounded w-full py-4 px-3 text-gray-700 text-sm leading-tight focus:outline-none focus:border-indigo-300" id="password" type="password" name="password" placeholder="***************">
         </div>
-        <p style="color:red;margin-bottom: 10px" id="error_msg"><?php if (isset($error)) {echo $error ;}?>
+        
+        <p style="color:red;margin-bottom: 10px" id="error_msg"><?php if (isset($error)) {echo $error;}?></p>
         <div class="mb-6">
-          <div class="grid sm:grid-cols-2 gap-4">
-            <div class="text-center sm:text-left">
-              <label>
-                <input type="checkbox" class="mr-2">
-                <span class="text-sm font-medium text-gray-700 ">Se souvenir de moi</span>
-              </label>
-            </div>
-            <div class="text-center sm:text-right">
-              <a href="#" class="text-indigo-600 font-medium text-sm duration-200 transition-colors hover:text-indigo-800">Mot de passe oublier ?</a>
-            </div>
-          </div>
         </div>
         <div class="mb-6">
           <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 shadow-lg text-white font-semibold text-sm py-3 px-0 rounded text-center w-full hover:bg-tertiary duration-200 transition-all">
-          Connexion
-          </button>
-          <button onclick="window.location.href = 'register.php'" style="margin-top: 10px;background-color: grey" class="bg-indigo-500 hover:bg-indigo-600 shadow-lg text-white font-semibold text-sm py-3 px-0 rounded text-center w-full hover:bg-tertiary duration-200 transition-all">
           Inscription
+          </button>
+
+          <button onclick="window.location.href = 'login.php'" style="margin-top: 10px;background-color: grey" class="bg-indigo-500 hover:bg-indigo-600 shadow-lg text-white font-semibold text-sm py-3 px-0 rounded text-center w-full hover:bg-tertiary duration-200 transition-all">
+          Connexion
           </button>
         </div>
     </div>
@@ -90,10 +93,11 @@ if (isset($_POST["mail"]) && isset($_POST["password"])) {
 </form>
 </body>
 
+
+
 <script>
 
-
-function verif_login(){
+function verif_register(){
 
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
@@ -106,6 +110,27 @@ function verif_login(){
 
   if(password == ""){
     error_msg.textContent = ("Merci de saisir un mot de passe valide");
+    return false;
+  }
+
+  if(password.length < 5){
+    error_msg.textContent = ("Votre mot de passe ne doit pas etre inférieur à 5 caractère !");
+    return false;
+  }
+
+  if(password.length > 255){
+    error_msg.textContent = ("Votre mot de passe est trop long !");
+    return false;
+  }
+
+  if(/[a-z]/.test(password) && /[A-Z]/.test(password) != true){
+    error_msg.textContent = ("Votre mot de passe doit contenir au moins une majuscule !");
+    return false;
+  }
+
+  var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if(format.test(password) != true){
+    error_msg.textContent = ("Votre mot de passe doit contenir au moins un caractère spécial !");
     return false;
   }
 
