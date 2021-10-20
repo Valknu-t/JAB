c34560f2f7fbb21837cd8779f4907abfd2f6e535
Valk <55887642+Valknu-t@users.noreply.github.com>
2021-10-20 09:59:49 +0000

    Update execution.js

--- execution.js
+++ execution.js
@@ -1,10 +1,13 @@
-import {nombre} from index;
+function nombre(){
+    chiffre = 4;
+    console.log(chiffre);
+    return chiffre;
+}
 
 function test(){
-    if (nombre == 4){
+    if (nombre() == 4){
         console.log("Le nombre est le bon !");
     }
-
 
     else{
         console.log("error");
