0d46558ca0d5ba1d4b392f308d9000a4c644dee8
Valk <55887642+Valknu-t@users.noreply.github.com>
2021-10-20 08:06:13 +0000

    Create execution.js

---
+++ execution.js
@@ -0,0 +1,14 @@
+import {nombre} from index;
+
+function test(){
+    if (nombre == 4){
+        console.log("Le nombre est le bon !");
+    }
+
+
+    else{
+        console.log("error");
+    }
+}
+
+test();
